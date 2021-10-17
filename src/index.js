a5d3aaddb8821e5b1ae3374f70e7c14a06fb882a
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-17 22:45:44 +0300

    Chaos progress

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
+
 import markup from './templates/country-info.handlebars';
 import debounce from 'lodash.debounce'
 
@@ -13,16 +14,60 @@ const { bodyEl, inputEl, formEl, countriesEl } = refs;
 
 inputEl.addEventListener('input', debounce(searchCountry, 500));
 
+
+
+
 function searchCountry(event) {
 event.preventDefault();
-  contry = event.target.value;
+  let contry = event.target.value;
   countriesEl.innerHTML = '';
 
-return fetch(`https://restcountries.com/v2/name/${event.target.value}`)
+return fetch(`https://restcountries.com/v2/name/${contry}`)
   .then(data => data.json())
-  .then(data => countriesEl.innerHTML = markup(data[0]));
+    .then(data => (countriesEl.innerHTML = markup(data[0])));
+  
+  // работает только с запросом и строкой запроса, возвращает данные
 }
 
+
+
+
+
+
+
+
+
+
+
+// class FetchCountries {
+//   constructor() {
+//     this._baseUrl = 'https://restcountries.com/v2/name/';
+//     this._searchQuery = 'ukraine';
+//   }
+
+//   set searchQuery(value) {
+//     return (this._searchQuery = value);
+//   }
+
+//   get searchQuery() {
+//     return this._searchQuery
+//   }
+
+//   getFetch() {
+//     let url = this._baseUrl + this._searchQuery;
+//     return fetch(url)
+//       .then(data => data.json())
+//       .then(data => data);;
+//   }
+
+
+// }
+
+// const fetchCountries = new FetchCountries()
+
+// console.log(fetchCountries.searchQuery);
+// fetchCountries.searchQuery = 'test'
+// console.log(fetchCountries.getFetch());
 
 
 

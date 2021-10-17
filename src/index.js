0c0f7e779b4e2b204e22f642c19f77e7cb8e98fb
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-17 18:26:30 +0300

    База

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import './sass/main.scss';
-
-const refs = {
-
-}
-
 /* 
 1. Создать рефы 
-2. Cоздать статичкскую разметку
-2. Создать динамическую разметку
+2. Cоздать статичкскую разметку 🤏
+2. Создать динамическую разметку 
 3. Прописать стили
 4. Создать запрос на API
 */
+
+import './sass/main.scss';
+import markup from './templates/country-info.handlebars';
+
+const refs = {
+  body: document.querySelector('body'),
+};
+
+console.log(markup());
+
+refs.body.insertAdjacentHTML('afterend', markup());
+
+fetch('https://restcountries.com/v2/name/peru')
+  .then(data => data.json())
+
+  .then(data => refs.body.insertAdjacentHTML('afterend', markup(data[0])));

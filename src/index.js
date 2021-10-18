f19fd2e55c3e12c9565d13f262d8c8ecf3fc7102
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-18 19:42:34 +0300

    Реализация разной разметки в зависимости от результата поиска

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 
-import markup from './templates/country-info.handlebars';
+import countryCardTemplate from './templates/country-info.hbs';
+import countriesListTemplate from './templates/countries-list.hbs'
 import debounce from 'lodash.debounce'
 
 const refs = {
@@ -12,64 +13,87 @@ const refs = {
 
 const { bodyEl, inputEl, formEl, countriesEl } = refs;
 
-// inputEl.addEventListener('input', debounce(searchCountry, 500));
+inputEl.addEventListener('input', debounce(searchCountry, 500));
 
+function searchCountry(event) {
+  event.preventDefault();
+  let searchQuery = event.target.value;
+  console.dir(event.target.value);
+  countriesEl.innerHTML = '';
 
-
-
-// function searchCountry(event) {
-// event.preventDefault();
-//   let contry = event.target.value;
-//   countriesEl.innerHTML = '';
-
-// return fetch(`https://restcountries.com/v2/name/${contry}`)
-//   .then(data => data.json())
-//     .then(data => (countriesEl.innerHTML = markup(data[0])));
-  
-//   // работает только с запросом и строкой запроса, возвращает данные
-// }
-
-
-
-
-
-
-
-
-
-
-
-class FetchCountries {
-  constructor() {
-    this._baseUrl = 'https://restcountries.com/v2/name/';
-    this._searchQuery = 'ukraine';
-  }
-
-  set searchQuery(value) {
-    return (this._searchQuery = value);
-  }
-
-  get searchQuery() {
-    return this._searchQuery
-  }
-
-  getFetch() {
-    let url = this._baseUrl + this._searchQuery;
-    return fetch(url)
-      .then(data => data.json())
-      .then(data => { return data });
-  }
-
+  fetchCountries(searchQuery).then(createCountryCard);
 
 }
 
-const fetchCountries = new FetchCountries()
+// fetchCountries(searchQuery);
+
+function fetchCountries(searchQuery) {
+return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
+  .then(data => data.json())
+}
+
+function createCountryCard(data) {
+  console.log(data.length);
+  if (data.length === 1) {
+    countriesEl.innerHTML = countryCardTemplate(data[0]);
+  } else {
+    countriesEl.innerHTML = countriesListTemplate(data);
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class FetchCountries {
+//   constructor() {
+//     this._baseUrl = 'https://restcountries.com/v2/name/';
+//     this._searchQuery = 'ukraine';
+//   }
+
+//   set searchQuery(value) {
+//     return (this._searchQuery = value);
+//   }
+
+//   get searchQuery() {
+//     return this._searchQuery
+//   }
+
+//   getFetch() {
+//     let url = this._baseUrl + this._searchQuery;
+//     return fetch(url)
+//       .then(data => data.json())
+//       .then(data => { return data });
+//   }
+
+
+// }
+
+// const fetchCountries = new FetchCountries()
 
 // console.log(fetchCountries.searchQuery);
 // console.log(fetchCountries.getFetch());
 
-const mark = fetchCountries.getFetch().then(data => markup(data));
-console.log(mark);
+// const mark = fetchCountries.getFetch().then(data => markup(data));
+// console.log(mark);
 
-countriesEl.innerHTML = mark;
+// countriesEl.innerHTML = mark;
 
